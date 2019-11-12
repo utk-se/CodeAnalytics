88d358d8b74ff79a453acfa90c756e15d4892916
Adam Short <adamconnorshort@gmail.com>
2019-11-12 15:59:03 -0500

    Progress

--- scrape.py
+++ scrape.py
@@ -2,10 +2,26 @@ import requests
 
 
 class PythonScraper:
-
     def getTopRepos(language, number):
-        r = requests.get("https://api.github.com/search/repositories?q=language:{}&sort=stars&order=desc&per_page={}".format(language, number))
-        return r.json()
+        links = []
+        num_pages = int(number / 100)
+        for i in range(0, num_pages):
+            r = requests.get("https://api.github.com/search/repositories?q=language:{}&sort=stars&order=desc&per_page={}&page={}".format(language, number, i))
+            if r.status_code != 200:
+                print("WARNING: Slow down your requests!!")
+                return None
+            for obj in r.json()["items"]:
+                links.append(obj["html_url"])
+        return links
 
+def main():
+    arr = PythonScraper.getTopRepos("python", 1000)
 
-obj = PythonScraper.getTopRepos("python", 100)
+    if arr is None:
+        return
+
+    for link in arr:
+        print(link)
+
+if __name__ == "__main__":
+    main()
