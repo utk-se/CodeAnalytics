f7680e15c5263e095d9ba0cd1c7efddfaaf4d8b4
Adam Short <adamconnorshort@gmail.com>
2019-11-14 09:48:12 -0500

    Refactoring

--- scrape.py
+++ scrape.py
@@ -2,25 +2,38 @@ import requests
 
 
 class PythonScraper:
-    def getTopRepos(language, number):
-        links = []
+
+    def __init__(self):
+        self.links = []
+
+    def getLinks(self):
+        return self.links
+
+    def getTopRepos(self, language, number):
         num_pages = int(number / 100)
         for i in range(0, num_pages):
             r = requests.get("https://api.github.com/search/repositories?q=language:{}&sort=stars&order=desc&per_page={}&page={}".format(language, number, i))
             if r.status_code != 200:
-                print("WARNING: Slow down your requests!!")
-                return None
+                raise Exception("WARNING: Please slow down your requests!")
             for obj in r.json()["items"]:
-                links.append(obj["html_url"])
-        return links
+                self.links.append(obj["html_url"])
 
 def main():
-    arr = PythonScraper.getTopRepos("python", 1000)
 
-    if arr is None:
+    scraper = PythonScraper()
+
+    try:
+        scraper.getTopRepos("python", 1000)
+    except Exception as error:
+        print(error)
+        return
+        
+    links = scraper.getLinks()
+
+    if links is None:
         return
 
-    for link in arr:
+    for link in links:
         print(link)
 
 if __name__ == "__main__":
