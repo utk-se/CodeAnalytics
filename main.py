215e3dd109121f5420fa1cb5d13b84c072f37a9a
Adam Short <adamconnorshort@gmail.com>
2019-11-17 16:51:01 -0500

    Don't run if keys aren't there

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
 
     if KEYS["client_id"] is "" or KEYS["client_secret"] is "":
         print("Please add your GitHub Client ID and Client Secret to keys.json")
+        return
 
     scraper = PythonScraper(KEYS)
 
