b5d3f66a401d9ab2f4efccd3e3271b21b389e1e0
Adam Short <adamconnorshort@gmail.com>
2019-11-18 09:44:48 -0500

    Progress

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import json
+import os
+import shutil
+
 from scraper import PythonScraper
+from git import Repo
 
 def main():
 
 
     KEY_FILE = "keys.json"
-    NUM_TO_SCRAPE = 500
+    REPOS_DIR = "repos/"
+    NUM_TO_SCRAPE = 10
 
     with open(KEY_FILE, 'r') as key_file:
         KEYS = json.load(key_file)
@@ -18,10 +23,20 @@ def main():
     scraper = PythonScraper(KEYS)
 
     scraper.getTopRepos("python", NUM_TO_SCRAPE)
-    scraper.getTopRepos("java", NUM_TO_SCRAPE)
-    scraper.getTopRepos("cpp", NUM_TO_SCRAPE)
+    # scraper.getTopRepos("java", NUM_TO_SCRAPE)
+    # scraper.getTopRepos("cpp", NUM_TO_SCRAPE)
 
     repos = scraper.getRepos()
+
+    for language in repos:
+        for repo in repos[language]:
+            repo_name = repo.split('/')[1]
+            repo_dest = REPOS_DIR + repo_name
+            print("Cloning {}".format(repo))
+            Repo.clone_from(repos[language][repo]["html_url"], repo_dest)
+            # Run analysis here...
+            print("Deleting {}".format(repo))
+            shutil.rmtree(repo_dest)
 
 if __name__ == "__main__":
     main()
